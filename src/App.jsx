9ff50da3a38fd4e5ac3fbf65bc65fa1998c8a159
Yongsik Yun <ysyun@woongjin.co.kr>
2025-07-28 14:42:01 +0900

    기본 레이아웃 구성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
-import { Button } from 'antd'
+import { Col, Layout, Row, Typography } from "antd";
 
-import './App.css'
+import { Content, Footer, Header } from "antd/es/layout/layout";
 
 function App() {
+  
   return (
-  <div className="App">
-    <Button type="primary">Button</Button>
-  </div>
-  )
+    <Layout style={{ minHeight: "100vh", minWidth: "100vw", maxWidth: "100vw"}}>
+      <Header style={{color:"#fff"}}>
+        헤더
+      </Header>
+      <Content>
+        <Row>
+          <Col span={24}>
+            <Typography.Title type="secondary" level={2}>
+              1줄 1칸
+            </Typography.Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <Typography.Title type="secondary" level={2}>
+              2줄 1칸
+            </Typography.Title>
+          </Col>
+          <Col span={12}>
+            <Typography.Title type="secondary" level={2}>
+              2줄 2칸
+            </Typography.Title>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={6}>
+            <Typography.Title type="secondary" level={2}>
+              3줄 1칸
+            </Typography.Title>
+          </Col>
+          <Col span={6}>
+            <Typography.Title type="secondary" level={2}>
+              3줄 2칸
+            </Typography.Title>
+          </Col>
+          <Col span={6}>
+            <Typography.Title type="secondary" level={2}>
+              3줄 3칸
+            </Typography.Title>
+          </Col>
+          <Col span={6}>
+            <Typography.Title type="secondary" level={2}>
+              3줄 4칸
+            </Typography.Title>
+          </Col>
+        </Row>
+      </Content>
+      <Footer>푸터</Footer>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
