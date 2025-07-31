d446ea7926c373a96a994f0153a4599cdd7e6b14
Yongsik Yun <comnori@gmail.com>
2025-07-31 17:16:53 +0900

    Add workspace page and route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Content, Footer, Header } from "antd/es/layout/layout";
 import { Link, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
+import Work from "./pages/Work.jsx";
 
 function App() {
   return (
@@ -15,12 +16,16 @@ function App() {
           <Menu.Item key="about">
             <Link to="/about">소개</Link>
           </Menu.Item>
+          <Menu.Item key="work">
+            <Link to="/work">작업화면</Link>
+          </Menu.Item>
         </Menu>
       </Header>
       <Content style={{ padding: "1rem" }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/work" element={<Work />} />
         </Routes>
       </Content>
       <Footer>푸터</Footer>
---
+++ src/pages/Work.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Work() {
+  return <></>;
+}
+
+export default Work;
