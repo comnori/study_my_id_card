8ea1ea535ace996ae08048109d3eed227e451fc3
Yongsik Yun <comnori@gmail.com>
2025-08-07 09:29:19 +0900

    chore: externalize and expand mock data

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Row, Col } from 'antd';
+import WorkSearch from './components/WorkSearch';
+import WorkResult from './components/WorkResult';
+import fakeData from './data/workData.json';
 
-function Work() {
-  return <></>;
-}
+const Work = () => {
+  const [data, setData] = useState([]);
+
+  const handleSearch = (values) => {
+    const filteredData = fakeData.filter((item) => {
+      const matchCompany = values.companyName
+        ? item.companyName.includes(values.companyName)
+        : true;
+      const matchBusiness = values.businessNumber
+        ? item.businessNumber.includes(values.businessNumber)
+        : true;
+      const matchManager = values.managerName
+        ? item.managerName.includes(values.managerName)
+        : true;
+      return matchCompany && matchBusiness && matchManager;
+    });
+    setData(filteredData);
+  };
+
+  return (
+    <Row gutter={[0, 16]}>
+      <Col span={24}>
+        <WorkSearch onSearch={handleSearch} />
+      </Col>
+      <Col span={24}>
+        <WorkResult data={data} />
+      </Col>
+    </Row>
+  );
+};
 
 export default Work;
